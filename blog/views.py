de44fda2506cac557ee6d73582172d0a8db437e7
Harshit <hvats16@gmai.com>
2021-11-09 10:18:41 +0530

    Comments Page

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from blog.models import Post, BlogComment
 from django.contrib import messages
 from django.contrib.auth.models import User
+from blog.templatetags import extras
 
 # Create your views here.
 def blogHome(request): 
@@ -11,8 +12,16 @@ def blogHome(request):
 
 def blogPost(request, slug): 
     post=Post.objects.filter(slug=slug).first()
-    comments= BlogComment.objects.filter(post=post)
-    context={'post':post, 'comments': comments , 'user':request.user}
+    comments= BlogComment.objects.filter(post=post, parent=None)
+    replies= BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict={}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno]=[reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+
+    context={'post':post, 'comments': comments, 'user': request.user, 'replyDict': replyDict}
     return render(request, "blog/blogPost.html", context)
 
 def postComment(request):
